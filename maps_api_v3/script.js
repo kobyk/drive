ebeb4cd938df45165994daaedcd4b0e35cdc344e
Koby <kkayden.bavc@gmail.com>
2013-04-10 18:44:33 -0700

    Added some more data an changed the zoom.

--- maps_api_v3/script.js
+++ maps_api_v3/script.js
@@ -39,7 +39,7 @@ var initGoogleMaps = function() {
 	// create map centered on SF
   var mapOptions = {
     center: new google.maps.LatLng(37.760401, -122.434731), // san francisco
-    zoom: 12,
+    zoom: 15,
     disableDefaultUI: true,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
